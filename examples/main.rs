de524af8cd1ff5752f387f2aeee3024539de2264
Gopinath Rangappa <160493+GopinathMR@users.noreply.github.com>
2024-12-11 14:23:31 -0800

    feat(CI/CD) : added github action CI/CD and fixed formatting to adhere to cargo fmt.

--- examples/main.rs
+++ examples/main.rs
@@ -1,11 +1,13 @@
 use mcp_ai::server::McpServer;
 use tracing::{dispatcher::with_default, info};
-use tracing_subscriber::{fmt, layer::SubscriberExt, EnvFilter, prelude::*};
-
+use tracing_subscriber::{fmt, layer::SubscriberExt, prelude::*, EnvFilter};
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::registry().with(fmt::layer()).with(EnvFilter::from_default_env()).init();
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
     info!("Starting MCP Server");
     let server = McpServer::new(8080, 8081);
     let _res = server.start().await.unwrap();
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 pub mod server;
 
 pub fn add(left: u64, right: u64) -> u64 {
-    
     left + right
 }
 
